a8c651946a481a013cda2be18da1399f1d16113e
Bhaskar Jyoti Deka <bhaskarjyotiy20@gmail.com>
2025-08-31 13:27:40 +0530

    Add new chatbot components and update UI

--- langgraph_database_backend.py
+++ langgraph_database_backend.py
@@ -2,9 +2,10 @@ from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.graph.message import add_messages
 from dotenv import load_dotenv
+import sqlite3
 
 load_dotenv()
 
@@ -26,8 +27,9 @@ def chat_node(state: ChatState):
     response=model.invoke(messages)
     return {'messages': [response]}
 
+conn=sqlite3.connect(database='chatbot.db', check_same_thread=False)
 
-checkpointer= InMemorySaver()
+checkpointer= SqliteSaver(conn=conn)
 
 graph=StateGraph(ChatState)
 
@@ -38,5 +40,9 @@ graph.add_edge('chat_node', END)
 
 chatbot=graph.compile(checkpointer=checkpointer)
 
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
 
-
+    return list(all_threads)
